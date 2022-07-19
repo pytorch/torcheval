1e46ad98dbe85712ab22c393cc45a68492349efc
Cynthia Yang <zixinyang@fb.com>
2022-07-19 12:16:07 -0700

    Add torcheval setup.py
    Summary: Add a setup.py to torcheval
    
    Reviewed By: DuYicong515
    
    Differential Revision: D37942584
    
    fbshipit-source-id: 89f52028d5bc52c86b50cac38926d9700405b442

---
+++ setup.py
@@ -0,0 +1,44 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import os
+
+from setuptools import find_packages, setup
+from torcheval import __version__
+
+
+def current_path(file_name):
+    return os.path.abspath(os.path.join(__file__, os.path.pardir, file_name))
+
+
+if __name__ == "__main__":
+    with open(current_path("README.md"), encoding="utf8") as f:
+        readme = f.read()
+
+    setup(
+        name="torcheval",
+        version=__version__,
+        author="torcheval team",
+        author_email="yicongd@fb.com",
+        description="A library for providing a simple interface to create new metrics and an easy-to-use toolkit for metric computations and checkpointing.",
+        long_description=readme,
+        long_description_content_type="text/markdown",
+        url="https://github.com/facebookresearch/torcheval",
+        license="BSD-3",
+        keywords=["pytorch", "evaluation", "metrics"],
+        python_requires=">=3.7",
+        packages=find_packages(),
+        zip_safe=True,
+        classifiers=[
+            "Development Status :: 2 - Pre-Alpha",
+            "Intended Audience :: Developers",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: BSD License",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.7",
+            "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        ],
+    )
