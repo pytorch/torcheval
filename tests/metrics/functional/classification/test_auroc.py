c21b22b1fcc077f47cc57957a85bf890b0849b81
Ning Li (Seattle) <ningli@fb.com>
2022-08-29 21:53:36 -0700

    AUROC update
    Summary: update AUROC with multi-task
    
    Reviewed By: DuYicong515
    
    Differential Revision: D39072059
    
    fbshipit-source-id: 965719f1b49077949f70ad0c1fa36f2f7739baef

--- tests/metrics/functional/classification/test_auroc.py
+++ tests/metrics/functional/classification/test_auroc.py
@@ -5,6 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import unittest
+from typing import Optional
 
 import torch
 from sklearn.metrics import roc_auc_score
@@ -17,15 +18,18 @@ class TestBinaryAUROC(unittest.TestCase):
         self,
         input: torch.Tensor,
         target: torch.Tensor,
+        num_tasks: int = 1,
+        compute_result: Optional[torch.Tensor] = None,
     ) -> None:
-        compute_result = torch.tensor(roc_auc_score(target, input))
+        if compute_result is None:
+            compute_result = torch.tensor(roc_auc_score(target, input))
         if torch.cuda.is_available():
             my_compute_result = binary_auroc(
-                input.to(device="cuda"), target.to(device="cuda")
+                input.to(device="cuda"), target.to(device="cuda"), num_tasks=num_tasks
             )
             compute_result = compute_result.to(device="cuda")
         else:
-            my_compute_result = binary_auroc(input, target)
+            my_compute_result = binary_auroc(input, target, num_tasks=num_tasks)
 
         torch.testing.assert_close(
             my_compute_result,
@@ -44,28 +48,31 @@ class TestBinaryAUROC(unittest.TestCase):
         target = torch.randint(high=2, size=(BATCH_SIZE,))
         self._test_auroc_with_input(input, target)
 
-        input = torch.rand(BATCH_SIZE)
-        target = torch.randint(high=2, size=(BATCH_SIZE,))
-        self._test_auroc_with_input(input, target)
+        input = torch.tensor([[1, 1, 1, 0], [0.1, 0.5, 0.7, 0.8]])
+        target = torch.tensor([[1, 0, 1, 0], [1, 0, 1, 1]])
+        self._test_auroc_with_input(
+            input,
+            target,
+            2,
+            torch.tensor([0.7500, 0.6666666666666666], dtype=torch.float64),
+        )
 
     def test_auroc_invalid_input(self) -> None:
-        with self.assertRaisesRegex(
-            ValueError,
-            "input should be a one-dimensional tensor, "
-            r"got shape torch.Size\(\[3, 2\]\).",
-        ):
-            binary_auroc(torch.rand(3, 2), torch.rand(3))
-
-        with self.assertRaisesRegex(
-            ValueError,
-            "target should be a one-dimensional tensor, "
-            r"got shape torch.Size\(\[3, 2\]\).",
-        ):
-            binary_auroc(torch.rand(3), torch.rand(3, 2))
-
         with self.assertRaisesRegex(
             ValueError,
             "The `input` and `target` should have the same shape, "
             r"got shapes torch.Size\(\[4\]\) and torch.Size\(\[3\]\).",
         ):
             binary_auroc(torch.rand(4), torch.rand(3))
+
+        with self.assertRaisesRegex(
+            ValueError,
+            "`num_tasks = 1`, `input` is expected to be one-dimensional tensor,",
+        ):
+            binary_auroc(torch.rand(4, 5), torch.rand(4, 5))
+
+        with self.assertRaisesRegex(
+            ValueError,
+            "`num_tasks = 2`, `input`'s shape is expected to be",
+        ):
+            binary_auroc(torch.rand(4, 5), torch.rand(4, 5), num_tasks=2)
--- torcheval/metrics/classification/auroc.py
+++ torcheval/metrics/classification/auroc.py
@@ -34,20 +34,32 @@ class BinaryAUROC(Metric[torch.Tensor]):
         >>> target = torch.tensor([1, 0, 1, 1])
         >>> metric.update(input, target)
         >>> metric.compute()
-        tensor(0.6667)
+        tensor([0.6667])
         >>> input = torch.tensor([1, 1, 1, 0])
         >>> target = torch.tensor([1, 1, 1, 0])
         >>> metric.update(input, target)
         >>> metric.compute()
-        tensor(0.9167)
+        tensor([1.0])
+        >>> metric = BinaryAUROC(num_tasks=2)
+        >>> input = torch.tensor([[1, 1, 1, 0], [0.1, 0.5, 0.7, 0.8]])
+        >>> target = torch.tensor([[1, 0, 1, 0], [1, 0, 1, 1]])
+        >>> metric.update(input, target)
+        >>> metric.compute()
+        tensor([0.7500, 0.6667])
     """
 
     def __init__(
         self: TAUROC,
         *,
+        num_tasks: int = 1,
         device: Optional[torch.device] = None,
     ) -> None:
         super().__init__(device=device)
+        if num_tasks < 1:
+            raise ValueError(
+                "`num_tasks` value should be greater than or equal to 1, but received {num_tasks}. "
+            )
+        self.num_tasks = num_tasks
         self._add_state("inputs", [])
         self._add_state("targets", [])
 
@@ -63,10 +75,10 @@ class BinaryAUROC(Metric[torch.Tensor]):
 
         Args:
             input (Tensor): Tensor of label predictions
-                It should be predicted label, probabilities or logits with shape of (n_sample, ).
-            target (Tensor): Tensor of ground truth labels with shape of (n_samples, ).
+                It should be predicted label, probabilities or logits with shape of (num_tasks, n_sample) or (n_sample, ).
+            target (Tensor): Tensor of ground truth labels with shape of (num_tasks, n_sample) or (n_sample, ).
         """
-        _auroc_update(input, target)
+        _auroc_update(input, target, self.num_tasks)
         self.inputs.append(input)
         self.targets.append(target)
         return self
@@ -75,14 +87,21 @@ class BinaryAUROC(Metric[torch.Tensor]):
     def compute(
         self: TAUROC,
     ) -> torch.Tensor:
-        return _auroc_compute(torch.cat(self.inputs), torch.cat(self.targets))
+        """
+        Return AUROC.  If no ``update()`` calls are made before
+        ``compute()`` is called, return an empty tensor.
+
+        Returns:
+            Tensor: The return value of AUROC for each task (num_tasks,).
+        """
+        return _auroc_compute(torch.cat(self.inputs, -1), torch.cat(self.targets, -1))
 
     @torch.inference_mode()
     def merge_state(self: TAUROC, metrics: Iterable[TAUROC]) -> TAUROC:
         for metric in metrics:
             if metric.inputs:
-                metric_inputs = torch.cat(metric.inputs).to(self.device)
-                metric_targets = torch.cat(metric.targets).to(self.device)
+                metric_inputs = torch.cat(metric.inputs, -1).to(self.device)
+                metric_targets = torch.cat(metric.targets, -1).to(self.device)
                 self.inputs.append(metric_inputs)
                 self.targets.append(metric_targets)
         return self
@@ -90,5 +109,5 @@ class BinaryAUROC(Metric[torch.Tensor]):
     @torch.inference_mode()
     def _prepare_for_merge_state(self: TAUROC) -> None:
         if self.inputs and self.targets:
-            self.inputs = [torch.cat(self.inputs)]
-            self.targets = [torch.cat(self.targets)]
+            self.inputs = [torch.cat(self.inputs, -1)]
+            self.targets = [torch.cat(self.targets, -1)]
