5b17b1e1a8e284b36a9e3856adda92f818ffcc68
Ananth Subramaniam <ananthsub@fb.com>
2022-08-31 17:44:06 -0700

    Add imports to tools/__init__.py for convenience
    Summary:
    As title, this lets users do this instead:
    ```
    from torcheval.tools import get_module_summary
    ```
    
    vs
    ```
    from torcheval.tools.module_summary import get_module_summary
    ```
    
    Differential Revision: D39195816
    
    fbshipit-source-id: 499f516e29e7a57d623b911e5ef939d7393d50c8

--- torcheval/tools/__init__.py
+++ torcheval/tools/__init__.py
@@ -3,3 +3,17 @@
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
+
+from torcheval.tools.module_summary import (
+    get_module_summary,
+    get_summary_table,
+    ModuleSummary,
+    prune_module_summary,
+)
+
+__all__ = [
+    "get_module_summary",
+    "get_summary_table",
+    "ModuleSummary",
+    "prune_module_summary",
+]
