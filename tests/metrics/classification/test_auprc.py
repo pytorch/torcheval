4518e7466e70636048288172b976ee58a491b7a9
William Hu <williamhu@meta.com>
2022-12-15 14:38:16 -0800

    Add Class Based code for multilabel AUPRC (#109)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torcheval/pull/109
    
    Adding class based code for the multilabel area under precision recall curve, also called average precision.
    
    Reviewed By: bobakfb
    
    Differential Revision:
    D41923743
    
    Privacy Context Container: L1098566
    
    fbshipit-source-id: b1a2bd84c6e842c760c5242c2aea3faa088bf08e

--- tests/metrics/classification/test_auprc.py
+++ tests/metrics/classification/test_auprc.py
@@ -6,7 +6,7 @@
 
 # pyre-ignore-all-errors[56]: Pyre was not able to infer the type of argument
 
-from typing import Tuple
+from typing import Optional, Tuple
 
 import numpy as np
 
@@ -15,6 +15,7 @@ import torch
 from sklearn.metrics import average_precision_score as sk_ap
 
 from torcheval.metrics import BinaryAUPRC, MulticlassAUPRC
+from torcheval.metrics.classification.auprc import MultilabelAUPRC
 from torcheval.utils.test_utils.metric_class_tester import (
     BATCH_SIZE,
     MetricClassTester,
@@ -225,7 +226,9 @@ class TestMulticlassAUPRC(MetricClassTester):
 
         compute_result = self._get_sklearn_equivalent(input, target, device)
         self.run_class_implementation_tests(
-            metric=MulticlassAUPRC(num_classes=num_classes, average=None),
+            metric=MulticlassAUPRC(
+                num_classes=num_classes, average=None, device=torch.device(device)
+            ),
             state_names={"inputs", "targets"},
             update_kwargs={
                 "input": input,
@@ -418,3 +421,281 @@ class TestMulticlassAUPRC(MetricClassTester):
             r"got torch.Size\(\[3, 4\]\) and num_classes=2.",
         ):
             metric.update(torch.rand(3, 4), torch.rand(3))
+
+
+class TestMultilabelAUPRC(MetricClassTester):
+    def _get_sklearn_equivalent(
+        self,
+        input: torch.Tensor,
+        target: torch.Tensor,
+        average: Optional[str] = "macro",
+        device: str = "cpu",
+    ) -> torch.Tensor:
+        # Convert input/target to sklearn style inputs
+        # run each task once at a time since no multi-task/multilabel
+        # available for sklearn
+        skinputs = input.flatten(0, 1).numpy()
+        sktargets = target.flatten(0, 1).numpy()
+        auprcs = []
+        for i in range(skinputs.shape[1]):
+            auprcs.append(np.nan_to_num(sk_ap(sktargets[:, i], skinputs[:, i])))
+        result = torch.tensor(auprcs, device=device)
+        if average == "macro":
+            result = torch.mean(result)
+        return result.to(torch.float32)
+
+    def _get_rand_inputs_multilabel(
+        self, num_updates: int, num_labels: int, batch_size: int
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        input = torch.rand(size=[num_updates, batch_size, num_labels])
+        targets = torch.randint(
+            low=0, high=2, size=[num_updates, batch_size, num_labels]
+        )
+        return input, targets
+
+    def _check_against_sklearn(
+        self,
+        input: torch.Tensor,
+        target: torch.Tensor,
+        num_labels: int,
+        average: Optional[str] = "macro",
+        num_updates: int = NUM_TOTAL_UPDATES,
+    ) -> None:
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        compute_result = self._get_sklearn_equivalent(
+            input,
+            target,
+            average,
+            device=device,
+        )
+        self.run_class_implementation_tests(
+            metric=MultilabelAUPRC(
+                num_labels=num_labels, average=average, device=torch.device(device)
+            ),
+            state_names={"inputs", "targets"},
+            update_kwargs={
+                "input": input,
+                "target": target,
+            },
+            compute_result=compute_result,
+            num_processes=NUM_PROCESSES,
+            num_total_updates=NUM_TOTAL_UPDATES,
+        )
+
+    def test_multilabel_auprc_class_none_average_with_good_inputs(self) -> None:
+        num_labels = 2
+        input, target = self._get_rand_inputs_multilabel(
+            NUM_TOTAL_UPDATES, num_labels, BATCH_SIZE
+        )
+        self._check_against_sklearn(input, target, num_labels, average=None)
+
+        num_labels = 4
+        input, target = self._get_rand_inputs_multilabel(
+            NUM_TOTAL_UPDATES, num_labels, BATCH_SIZE
+        )
+        self._check_against_sklearn(input, target, num_labels, average=None)
+
+        num_labels = 8
+        input, target = self._get_rand_inputs_multilabel(
+            NUM_TOTAL_UPDATES, num_labels, BATCH_SIZE
+        )
+        self._check_against_sklearn(input, target, num_labels, average=None)
+
+    def test_multilabel_auprc_class_macro_average_with_good_inputs(self) -> None:
+        num_labels = 2
+        input, target = self._get_rand_inputs_multilabel(
+            NUM_TOTAL_UPDATES, num_labels, BATCH_SIZE
+        )
+        self._check_against_sklearn(input, target, num_labels, average="macro")
+
+        num_labels = 4
+        input, target = self._get_rand_inputs_multilabel(
+            NUM_TOTAL_UPDATES, num_labels, BATCH_SIZE
+        )
+        self._check_against_sklearn(input, target, num_labels, average="macro")
+
+        num_labels = 8
+        input, target = self._get_rand_inputs_multilabel(
+            NUM_TOTAL_UPDATES, num_labels, BATCH_SIZE
+        )
+        self._check_against_sklearn(input, target, num_labels, average="macro")
+
+    def test_multilabel_auprc_docstring_examples(self) -> None:
+        metric = MultilabelAUPRC(num_labels=3, average=None)
+        input = torch.tensor(
+            [
+                [0.75, 0.05, 0.35],
+                [0.45, 0.75, 0.05],
+                [0.05, 0.55, 0.75],
+                [0.05, 0.65, 0.05],
+            ]
+        )
+        target = torch.tensor([[1, 0, 1], [0, 0, 0], [0, 1, 1], [1, 1, 1]])
+        metric.update(input, target)
+        result = metric.compute()
+        expected = torch.tensor([0.7500, 0.5833, 0.9167])
+
+        torch.testing.assert_close(
+            result,
+            expected,
+            equal_nan=True,
+            atol=1e-4,
+            rtol=1e-3,
+        )
+
+        metric = MultilabelAUPRC(num_labels=3, average="macro")
+        input = torch.tensor([[0.75, 0.05, 0.35], [0.05, 0.55, 0.75]])
+        target = torch.tensor([[1, 0, 1], [0, 1, 1]])
+        metric.update(input, target)
+        result = metric.compute()
+        expected = torch.tensor(1.0)
+        torch.testing.assert_close(
+            result,
+            expected,
+            equal_nan=True,
+            atol=1e-4,
+            rtol=1e-3,
+        )
+
+        input = torch.tensor([[0.45, 0.75, 0.05], [0.05, 0.65, 0.05]])
+        target = torch.tensor([[0, 0, 0], [1, 1, 1]])
+        metric.update(input, target)
+        result = metric.compute()
+        expected = torch.tensor(0.7500)
+        torch.testing.assert_close(
+            result,
+            expected,
+            equal_nan=True,
+            atol=1e-4,
+            rtol=1e-3,
+        )
+
+        metric = MultilabelAUPRC(num_labels=3, average=None)
+        input = torch.tensor([[0.1, 0, 0], [0, 1, 0], [0.1, 0.2, 0.7], [0, 0, 1]])
+        target = torch.tensor([[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 0, 1]])
+        metric.update(input, target)
+        result = metric.compute()
+        expected = torch.tensor([0.5000, 1.0000, 1.0000])
+        torch.testing.assert_close(
+            result,
+            expected,
+            equal_nan=True,
+            atol=1e-4,
+            rtol=1e-3,
+        )
+
+    def test_multilabel_auprc_options(self) -> None:
+        # average = "macro" (implicit)
+        num_labels = 4
+        input, target = self._get_rand_inputs_multilabel(
+            NUM_TOTAL_UPDATES, num_labels, BATCH_SIZE
+        )
+        self._check_against_sklearn(input, target, num_labels)
+
+        compute_result = self._get_sklearn_equivalent(input, target, device="cpu")
+
+        self.run_class_implementation_tests(
+            metric=MultilabelAUPRC(num_labels=num_labels),
+            state_names={"inputs", "targets"},
+            update_kwargs={
+                "input": input,
+                "target": target,
+            },
+            compute_result=compute_result,
+            num_processes=NUM_PROCESSES,
+            num_total_updates=NUM_TOTAL_UPDATES,
+        )
+
+        # average = "macro"
+        num_labels = 4
+        input, target = self._get_rand_inputs_multilabel(
+            NUM_TOTAL_UPDATES, num_labels, BATCH_SIZE
+        )
+        self._check_against_sklearn(input, target, num_labels, average="macro")
+
+        compute_result = self._get_sklearn_equivalent(
+            input, target, average="macro", device="cpu"
+        )
+
+        self.run_class_implementation_tests(
+            metric=MultilabelAUPRC(num_labels=num_labels, average="macro"),
+            state_names={"inputs", "targets"},
+            update_kwargs={
+                "input": input,
+                "target": target,
+            },
+            compute_result=compute_result,
+            num_processes=NUM_PROCESSES,
+            num_total_updates=NUM_TOTAL_UPDATES,
+        )
+
+        # average = None (explicit)
+        num_labels = 4
+        input, target = self._get_rand_inputs_multilabel(
+            NUM_TOTAL_UPDATES, num_labels, BATCH_SIZE
+        )
+        self._check_against_sklearn(input, target, num_labels)
+
+        compute_result = self._get_sklearn_equivalent(
+            input, target, average=None, device="cpu"
+        )
+
+        self.run_class_implementation_tests(
+            metric=MultilabelAUPRC(num_labels=num_labels, average=None),
+            state_names={"inputs", "targets"},
+            update_kwargs={
+                "input": input,
+                "target": target,
+            },
+            compute_result=compute_result,
+            num_processes=NUM_PROCESSES,
+            num_total_updates=NUM_TOTAL_UPDATES,
+        )
+        self.run_class_implementation_tests(
+            metric=MultilabelAUPRC(num_labels=num_labels, average="none"),
+            state_names={"inputs", "targets"},
+            update_kwargs={
+                "input": input,
+                "target": target,
+            },
+            compute_result=compute_result,
+            num_processes=NUM_PROCESSES,
+            num_total_updates=NUM_TOTAL_UPDATES,
+        )
+
+    def test_multilabel_auprc_class_invalid_input(self) -> None:
+        with self.assertRaisesRegex(
+            ValueError, "`average` was not in the allowed value of .*, got micro."
+        ):
+            MultilabelAUPRC(num_labels=4, average="micro")
+
+        with self.assertRaisesRegex(ValueError, "`num_labels` has to be at least 2."):
+            MultilabelAUPRC(num_labels=1)
+
+        metric = MultilabelAUPRC(num_labels=3, average="macro")
+        with self.assertRaisesRegex(
+            ValueError,
+            "Expected both input.shape and target.shape to have the same shape"
+            r" but got torch.Size\(\[4, 2\]\) and torch.Size\(\[4, 3\]\).",
+        ):
+            metric.update(torch.rand(4, 2), torch.randint(high=2, size=(4, 3)))
+
+        with self.assertRaisesRegex(
+            ValueError,
+            r"input should have shape of \(num_sample, num_labels\), "
+            r"got torch.Size\(\[4, 2\]\) and num_labels=3.",
+        ):
+            metric.update(torch.rand(4, 2), torch.randint(high=2, size=(4, 2)))
+
+        with self.assertRaisesRegex(
+            ValueError,
+            r"input should be a two-dimensional tensor, got shape torch.Size\(\[4\]\).",
+        ):
+            metric.update(torch.rand(4), torch.randint(high=2, size=(4,)))
+
+        with self.assertRaisesRegex(
+            ValueError,
+            r"input should be a two-dimensional tensor, got shape torch.Size\(\[4, 3, 2\]\).",
+        ):
+            metric.update(torch.rand(4, 3, 2), torch.randint(high=2, size=(4, 3, 2)))
--- torcheval/metrics/classification/auprc.py
+++ torcheval/metrics/classification/auprc.py
@@ -16,12 +16,16 @@ from torcheval.metrics.functional.classification.auprc import (
     _multiclass_auprc_compute,
     _multiclass_auprc_param_check,
     _multiclass_auprc_update_input_check,
+    _multilabel_auprc_compute,
+    _multilabel_auprc_param_check,
+    _multilabel_auprc_update_input_check,
 )
 from torcheval.metrics.metric import Metric
 
 
 TBinaryAUPRC = TypeVar("TBinaryAUPRC", bound=Metric[torch.Tensor])
 TMulticlassAUPRC = TypeVar("TMulticlassAUPRC", bound=Metric[torch.Tensor])
+TMultilabelAUPRC = TypeVar("TMultilabelAUPRC", bound=Metric[torch.Tensor])
 
 
 class BinaryAUPRC(Metric[torch.Tensor]):
@@ -179,7 +183,7 @@ class MulticlassAUPRC(Metric[torch.Tensor]):
     Examples::
 
         >>> import torch
-        >>> from torcheval.metrics import MulticlassAUROC
+        >>> from torcheval.metrics import MulticlassAUPRC
         >>> metric = MulticlassAUPRC(num_classes=3)
         >>> input = torch.tensor([[0.1, 0.1, 0.1], [0.5, 0.5, 0.5], [0.7, 0.7, 0.7], [0.8, 0.8, 0.8]])
         >>> target = torch.tensor([0, 2, 1, 1])
@@ -275,6 +279,146 @@ class MulticlassAUPRC(Metric[torch.Tensor]):
 
     @torch.inference_mode()
     def _prepare_for_merge_state(self: TMulticlassAUPRC) -> None:
+        if self.inputs and self.targets:
+            self.inputs = [torch.cat(self.inputs)]
+            self.targets = [torch.cat(self.targets)]
+
+
+class MultilabelAUPRC(Metric[torch.Tensor]):
+    """
+    Compute AUPRC, also called Average Precision, which is the area under the Precision-Recall Curve, for multilabel classification.
+
+    Precision is defined as :math:`\frac{T_p}{T_p+F_p}`, it is the probability that a positive prediction from the model is a true positive.
+    Recall is defined as :math:`\frac{T_p}{T_p+F_n}`, it is the probability that a true positive is predicted to be positive by the model.
+
+    The precision-recall curve plots the recall on the x axis and the precision on the y axis, both of which are bounded between 0 and 1.
+    This function returns the area under that graph. If the area is near one, the model supports a threshold which correctly identifies
+    a high percentage of true positives while also rejecting enough false examples so that most of the true predictions are true positives.
+
+    In the multilabel version of AUPRC, the input and target tensors are 2-dimensional. The rows of each tensor are associated with a particular example and the columns are associated with a particular class.
+
+    For the target tensor, the entry of the r'th row and c'th column (r and c are 0-indexed) is 1 if the r'th example belongs to the c'th class, and 0 if not. For the input tensor, the entry in the same position is the output of the classification model prediciting the inclusion of the r'th example in the c'th class.
+    Note that in the multilabel setting, multiple labels are allowed to apply to a single sample. This stands in contrast to the multiclass
+    sample, in which there may be more than 2 distinct classes but each sample must have exactly one class.
+
+    The results of N label multilabel auprc without an average is equivalent to binary auprc with N tasks if:
+    1. the `input` is transposed, in binary labelification examples are associated with columns, whereas they are associated with rows in multilabel classification.
+    2. the `target` is transposed for the same reason
+
+    See examples below for more details on the connection between Multilabel and Binary AUPRC.
+
+    The functional version of this metric is :func:`torcheval.metrics.functional.multilabel_auprc`.
+
+    Args:
+        num_labels (int): Number of labels.
+        average (str, optional):
+
+          - ``'macro'`` [default]:
+                Calculate metrics for each label separately, and return their unweighted mean.
+          - ``None``:
+                Calculate the metric for each label separately, and return
+                the metric for every label.
+
+    Examples::
+
+        >>> import torch
+        >>> from torcheval.metrics import MultilabelAUPRC
+        >>> metric = MultilabelAUPRC(num_labels=3, average=None)
+        >>> input = torch.tensor([[0.75, 0.05, 0.35], [0.45, 0.75, 0.05], [0.05, 0.55, 0.75], [0.05, 0.65, 0.05]])
+        >>> target = torch.tensor([[1, 0, 1], [0, 0, 0], [0, 1, 1], [1, 1, 1]])
+        >>> metric.update(input, target)
+        >>> metric.compute()
+        tensor([0.7500, 0.5833, 0.9167])
+
+        >>> metric = MultilabelAUPRC(num_labels=3, average='macro')
+        >>> input = torch.tensor([[0.75, 0.05, 0.35], [0.05, 0.55, 0.75]])
+        >>> target = torch.tensor([[1, 0, 1], [0, 1, 1]])
+        >>> metric.update(input, target)
+        >>> metric.compute()
+        tensor(1.)
+        >>> input = torch.tensor([[0.45, 0.75, 0.05], [0.05, 0.65, 0.05]])
+        >>> target = torch.tensor([[0, 0, 0], [1, 1, 1]])
+        >>> metric.update(input, target)
+        >>> metric.compute()
+        tensor(0.7500)
+
+        Connection to BinaryAUPRC
+        >>> metric = MultilabelAUPRC(num_labels=3, average=None)
+        >>> input = torch.tensor([[0.1, 0, 0], [0, 1, 0], [0.1, 0.2, 0.7], [0, 0, 1]])
+        >>> target = torch.tensor([[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 0, 1]])
+        >>> metric.update(input, target)
+        >>> metric.compute()
+        tensor([0.5000, 1.0000, 1.0000])
+
+        the above is equivalent to
+        >>> from torcheval.metrics import BinaryAUPRC
+        >>> metric = BinaryAUPRC(num_tasks=3)
+        >>> input = torch.tensor([[0.1, 0, 0.1, 0], [0, 1, 0.2, 0], [0, 0, 0.7, 1]])
+        >>> target = torch.tensor([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 1]])
+        >>> metric.update(input, target)
+        >>> metric.compute()
+        tensor([0.5000, 1.0000, 1.0000])
+    """
+
+    def __init__(
+        self: TMultilabelAUPRC,
+        *,
+        num_labels: int,
+        average: Optional[str] = "macro",
+        device: Optional[torch.device] = None,
+    ) -> None:
+        super().__init__(device=device)
+        _multilabel_auprc_param_check(num_labels, average)
+        self.num_labels = num_labels
+        self.average = average
+        self._add_state("inputs", [])
+        self._add_state("targets", [])
+
+    @torch.inference_mode()
+    # pyre-ignore[14]: inconsistent override on *_:Any, **__:Any
+    def update(
+        self: TMultilabelAUPRC,
+        input: torch.Tensor,
+        target: torch.Tensor,
+    ) -> TMultilabelAUPRC:
+        """
+        Update states with the ground truth labels and predictions.
+
+        Args:
+            input (Tensor): Tensor of label predictions
+                It should be probabilities or logits with shape of (n_sample, n_label).
+            target (Tensor): Tensor of ground truth labels with shape of (n_samples, n_label).
+        """
+        _multilabel_auprc_update_input_check(input, target, self.num_labels)
+        self.inputs.append(input)
+        self.targets.append(target)
+        return self
+
+    @torch.inference_mode()
+    def compute(
+        self: TMultilabelAUPRC,
+    ) -> torch.Tensor:
+        return _multilabel_auprc_compute(
+            torch.cat(self.inputs),
+            torch.cat(self.targets),
+            self.num_labels,
+            self.average,
+        )
+
+    @torch.inference_mode()
+    def merge_state(
+        self: TMultilabelAUPRC, metrics: Iterable[TMultilabelAUPRC]
+    ) -> TMultilabelAUPRC:
+        for metric in metrics:
+            if metric.inputs:
+                metric_inputs = torch.cat(metric.inputs).to(self.device)
+                metric_targets = torch.cat(metric.targets).to(self.device)
+                self.inputs.append(metric_inputs)
+                self.targets.append(metric_targets)
+        return self
+
+    @torch.inference_mode()
+    def _prepare_for_merge_state(self: TMultilabelAUPRC) -> None:
         if self.inputs and self.targets:
             self.inputs = [torch.cat(self.inputs)]
             self.targets = [torch.cat(self.targets)]
