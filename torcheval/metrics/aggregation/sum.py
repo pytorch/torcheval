Fix torcheval not enough precision issue (#116)
Summary:
Pull Request resolved: https://github.com/pytorch/torcheval/pull/116

Fix torcheval not enough precision issue

Differential Revision: D42360657

fbshipit-source-id: 32f92702ded3dd6e01579f92c5d56c41dece57dc

torcheval/metrics/aggregation/sum.py
@@ -52,7 +52,9 @@ class Sum(Metric[torch.Tensor]):
         device: Optional[torch.device] = None,
     ) -> None:
         super().__init__(device=device)
-        self._add_state("weighted_sum", torch.tensor(0.0, device=self.device))
+        self._add_state(
+            "weighted_sum", torch.tensor(0.0, device=self.device, dtype=torch.float64)
+        )
 
     @torch.inference_mode()
     # pyre-ignore[14]: inconsistent override on *_:Any, **__:Any