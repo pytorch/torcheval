59e0cad54f726c7da70deded8d7b7b4dccc4b71e
William Hu <williamhu@meta.com>
2023-04-05 13:58:52 -0700

    Class Version of Multiclass Binned AUPRC Library (#143)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torcheval/pull/143
    
    We add the class version of the Multiclass Binned AUPRC library.
    
    Reviewed By: bobakfb
    
    Differential Revision: D44654082
    
    fbshipit-source-id: aec180eab411c7e9a349a9460b536c7e0296ab57

--- tests/metrics/classification/test_binned_auprc.py
+++ tests/metrics/classification/test_binned_auprc.py
@@ -8,6 +8,7 @@ from typing import List, Tuple, Union
 
 import torch
 from torcheval.metrics import BinaryBinnedAUPRC
+from torcheval.metrics.classification.binned_auprc import MulticlassBinnedAUPRC
 from torcheval.utils.test_utils.metric_class_tester import (
     BATCH_SIZE,
     MetricClassTester,
@@ -187,5 +188,182 @@ class TestBinaryBinnedAUPRC(MetricClassTester):
             r"Last value in `threshold` should be 1.",
         ):
             metric = BinaryBinnedAUPRC(
+                threshold=torch.tensor([0.0, 0.2, 0.5, 0.9]),
+            )
+
+
+class TestMulticlassBinnedAUPRC(MetricClassTester):
+    def test_auprc_class_base(self) -> None:
+        num_classes = 4
+        threshold = 5
+        torch.manual_seed(123)
+        input = 10 * torch.rand(NUM_TOTAL_UPDATES, BATCH_SIZE, num_classes)
+        input = input.abs() / input.abs().sum(dim=-1, keepdim=True)
+        target = torch.randint(high=num_classes, size=(NUM_TOTAL_UPDATES, BATCH_SIZE))
+
+        compute_result = (
+            torch.tensor(0.2522818148136139),
+            torch.tensor([0.0000, 0.2500, 0.5000, 0.7500, 1.0000]),
+        )
+
+        self.run_class_implementation_tests(
+            metric=MulticlassBinnedAUPRC(num_classes=num_classes, threshold=threshold),
+            state_names={"inputs", "targets"},
+            update_kwargs={
+                "input": input,
+                "target": target,
+            },
+            compute_result=compute_result,
+        )
+
+    def test_auprc_average_options(self) -> None:
+        input = torch.tensor(
+            [
+                [[0.16, 0.04, 0.8]],
+                [[0.1, 0.7, 0.2]],
+                [[0.16, 0.8, 0.04]],
+                [[0.16, 0.04, 0.8]],
+            ]
+        )
+        target = torch.tensor([[0], [0], [1], [2]])
+
+        self.run_class_implementation_tests(
+            metric=MulticlassBinnedAUPRC(num_classes=3, threshold=5, average="macro"),
+            state_names={"inputs", "targets"},
+            update_kwargs={
+                "input": input,
+                "target": target,
+            },
+            num_total_updates=4,
+            num_processes=2,
+            compute_result=(
+                torch.tensor(2 / 3),
+                torch.tensor([0.0000, 0.2500, 0.5000, 0.7500, 1.0000]),
+            ),
+        )
+
+        self.run_class_implementation_tests(
+            metric=MulticlassBinnedAUPRC(num_classes=3, threshold=5, average=None),
+            state_names={"inputs", "targets"},
+            update_kwargs={
+                "input": input,
+                "target": target,
+            },
+            num_total_updates=4,
+            num_processes=2,
+            compute_result=(
+                torch.tensor([0.5000, 1.0000, 0.5000]),
+                torch.tensor([0.0000, 0.2500, 0.5000, 0.7500, 1.0000]),
+            ),
+        )
+
+    def test_auprc_class_update_input_shape_different(self) -> None:
+        torch.manual_seed(123)
+        num_classes = 3
+        update_input = [
+            torch.rand(5, num_classes),
+            torch.rand(8, num_classes),
+            torch.rand(2, num_classes),
+            torch.rand(5, num_classes),
+        ]
+        update_input = [
+            input.abs() / input.abs().sum(dim=-1, keepdim=True)
+            for input in update_input
+        ]
+        update_target = [
+            torch.randint(high=num_classes, size=(5,)),
+            torch.randint(high=num_classes, size=(8,)),
+            torch.randint(high=num_classes, size=(2,)),
+            torch.randint(high=num_classes, size=(5,)),
+        ]
+        compute_result = (
+            torch.tensor(0.372433333333333),
+            torch.tensor([0.0000, 0.2500, 0.5000, 0.7500, 1.0000]),
+        )
+
+        self.run_class_implementation_tests(
+            metric=MulticlassBinnedAUPRC(num_classes=num_classes, threshold=5),
+            state_names={"inputs", "targets"},
+            update_kwargs={
+                "input": update_input,
+                "target": update_target,
+            },
+            compute_result=compute_result,
+            num_total_updates=4,
+            num_processes=2,
+        )
+
+    def test_auprc_class_invalid_input(self) -> None:
+        with self.assertRaisesRegex(
+            ValueError, "`average` was not in the allowed value of .*, got micro."
+        ):
+            metric = MulticlassBinnedAUPRC(num_classes=4, average="micro")
+
+        with self.assertRaisesRegex(ValueError, "`num_classes` has to be at least 2."):
+            metric = MulticlassBinnedAUPRC(num_classes=1)
+
+        with self.assertRaisesRegex(
+            ValueError,
+            "The `input` and `target` should have the same first dimension, "
+            r"got shapes torch.Size\(\[4, 2\]\) and torch.Size\(\[3\]\).",
+        ):
+            metric = MulticlassBinnedAUPRC(num_classes=3)
+            metric.update(torch.rand(4, 2), torch.rand(3))
+
+        with self.assertRaisesRegex(
+            ValueError,
+            "target should be a one-dimensional tensor, "
+            r"got shape torch.Size\(\[3, 2\]\).",
+        ):
+            metric = MulticlassBinnedAUPRC(num_classes=2)
+            metric.update(torch.rand(3, 2), torch.rand(3, 2))
+
+        with self.assertRaisesRegex(
+            ValueError,
+            r"input should have shape of \(num_sample, num_classes\), "
+            r"got torch.Size\(\[3, 4\]\) and num_classes=2.",
+        ):
+            metric = MulticlassBinnedAUPRC(num_classes=2)
+            metric.update(torch.rand(3, 4), torch.rand(3))
+
+        with self.assertRaisesRegex(
+            ValueError, "The `threshold` should be a sorted tensor."
+        ):
+            metric = MulticlassBinnedAUPRC(
+                num_classes=4, threshold=torch.tensor([0.1, 0.2, 0.5, 0.7, 0.6])
+            )
+
+        with self.assertRaisesRegex(
+            ValueError,
+            r"The values in `threshold` should be in the range of \[0, 1\].",
+        ):
+            metric = MulticlassBinnedAUPRC(
+                num_classes=4, threshold=torch.tensor([-0.1, 0.2, 0.5, 0.7])
+            )
+
+        with self.assertRaisesRegex(
+            ValueError,
+            r"`threshold` should be 1-dimensional, but got 2D tensor.",
+        ):
+            metric = MulticlassBinnedAUPRC(
+                num_classes=4,
+                threshold=torch.tensor([[-0.1, 0.2, 0.5, 0.7], [0.0, 0.4, 0.6, 1.0]]),
+            )
+
+        with self.assertRaisesRegex(
+            ValueError,
+            r"First value in `threshold` should be 0.",
+        ):
+            metric = MulticlassBinnedAUPRC(
+                num_classes=4,
+                threshold=torch.tensor([0.1, 0.2, 0.5, 1.0]),
+            )
+
+        with self.assertRaisesRegex(
+            ValueError,
+            r"Last value in `threshold` should be 1.",
+        ):
+            metric = MulticlassBinnedAUPRC(
+                num_classes=4,
                 threshold=torch.tensor([0.0, 0.2, 0.5, 0.9]),
             )
--- torcheval/metrics/classification/binned_auprc.py
+++ torcheval/metrics/classification/binned_auprc.py
@@ -14,6 +14,9 @@ from torcheval.metrics.functional.classification.binned_auprc import (
     _binary_binned_auprc_compute,
     _binary_binned_auprc_param_check,
     _binary_binned_auprc_update_input_check,
+    _multiclass_binned_auprc_compute,
+    _multiclass_binned_auprc_param_check,
+    _multiclass_binned_auprc_update_input_check,
     DEFAULT_NUM_THRESHOLD,
 )
 from torcheval.metrics.functional.classification.binned_precision_recall_curve import (
@@ -23,11 +26,12 @@ from torcheval.metrics.metric import Metric
 
 
 TBinaryBinnedAUPRC = TypeVar("TBinaryBinnedAUPRC")
+TMulticlassBinnedAUPRC = TypeVar("TMulticlassBinnedAUPRC")
 
 
 class BinaryBinnedAUPRC(Metric[Tuple[torch.Tensor, torch.Tensor]]):
     """
-    Compute AUPRC, which is the area under the Precision Recall Curve, for binary classification.
+    Compute Binned AUPRC, which is the area under the binned version of the Precision Recall Curve, for binary classification.
     Its functional version is :func:`torcheval.metrics.functional.binary_binned_auprc`.
 
     Args:
@@ -141,3 +145,129 @@ class BinaryBinnedAUPRC(Metric[Tuple[torch.Tensor, torch.Tensor]]):
         if self.inputs and self.targets:
             self.inputs = [torch.cat(self.inputs, -1)]
             self.targets = [torch.cat(self.targets, -1)]
+
+
+class MulticlassBinnedAUPRC(Metric[Tuple[torch.Tensor, torch.Tensor]]):
+    """
+    Compute Binned AUPRC, which is the area under the binned version of the Precision Recall Curve, for multiclass classification.
+    Its functional version is :func:`torcheval.metrics.functional.multiclass_binned_auprc`.
+
+    Args:
+        num_classes (int): Number of classes.
+        threshold (Tensor, int, List[float]): Either an integer representing the number of bins, a list of thresholds, or a tensor of thresholds.
+                    The same thresholds will be used for all tasks.
+                    If `threshold` is a tensor, it must be 1D.
+                    If list or tensor is given, the first element must be 0 and the last must be 1.
+        average (str, optional):
+            - ``'macro'`` [default]:
+                Calculate metrics for each class separately, and return their unweighted mean.
+            - ``None``:
+                Calculate the metric for each class separately, and return
+                the metric for every class.
+
+
+    Examples::
+
+        >>> import torch
+        >>> from torcheval.metrics import MulticlassBinnedAUPRC
+        >>> input = torch.tensor([[0.1, 0.2, 0.1], [0.4, 0.2, 0.1], [0.6, 0.1, 0.2], [0.4, 0.2, 0.3], [0.6, 0.2, 0.4]])
+        >>> target = torch.tensor([0, 1, 2, 1, 0])
+        >>> metric = MulticlassBinnedAUPRC(num_classes=3, threshold=5, average='macro')
+        >>> metric.update(input, target)
+        >>> metric.compute()
+        tensor(0.35)
+        >>> metric = MulticlassBinnedAUPRC(num_classes=3, threshold=5, average=None)
+        >>> metric.update(input, target)
+        >>> metric.compute()
+        tensor([0.4500, 0.4000, 0.2000])
+        >>> input = torch.tensor([[0.1, 0.2, 0.1, 0.4], [0.4, 0.2, 0.1, 0.7], [0.6, 0.1, 0.2, 0.4], [0.4, 0.2, 0.3, 0.2], [0.6, 0.2, 0.4, 0.5]])
+        >>> target = torch.tensor([0, 1, 2, 1, 0])
+        >>> threshold = torch.tensor([0.0, 0.1, 0.4, 0.7, 0.8, 1.0])
+        >>> metric = MulticlassBinnedAUPRC(input, target, num_classes=4, threshold=threshold, average='macro')
+        >>> metric.update(input, target)
+        >>> metric.compute()
+        tensor(0.24375)
+        >>> metric = MulticlassBinnedAUPRC(input, target, num_classes=4, threshold=threshold, average='macro')
+        >>> metric.update(input, target)
+        >>> metric.compute()
+        tensor([0.3250, 0.2000, 0.2000, 0.2500])
+    """
+
+    def __init__(
+        self: TMulticlassBinnedAUPRC,
+        *,
+        num_classes: int,
+        threshold: Union[int, List[float], torch.Tensor] = DEFAULT_NUM_THRESHOLD,
+        average: Optional[str] = "macro",
+        device: Optional[torch.device] = None,
+    ) -> None:
+        super().__init__(device=device)
+        threshold = _create_threshold_tensor(
+            threshold,
+            self.device,
+        )
+        _multiclass_binned_auprc_param_check(num_classes, threshold, average)
+        self.num_classes = num_classes
+        self.threshold = threshold
+        self.average = average
+        self._add_state("inputs", [])
+        self._add_state("targets", [])
+
+    @torch.inference_mode()
+    # pyre-ignore[14]: inconsistent override on *_:Any, **__:Any
+    def update(
+        self: TMulticlassBinnedAUPRC,
+        input: torch.Tensor,
+        target: torch.Tensor,
+    ) -> TMulticlassBinnedAUPRC:
+        """
+        Update states with the ground truth labels and predictions.
+
+        Args:
+            input (Tensor): Tensor of label predictions
+                It should be predicted label, probabilities or logits with shape of (num_tasks, n_sample) or (n_sample, ).
+            target (Tensor): Tensor of ground truth labels with shape of (num_tasks, n_sample) or (n_sample, ).
+        """
+        _multiclass_binned_auprc_update_input_check(input, target, self.num_classes)
+        self.inputs.append(input)
+        self.targets.append(target)
+        return self
+
+    @torch.inference_mode()
+    def compute(
+        self: TMulticlassBinnedAUPRC,
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Return Binned_AUPRC.  If no ``update()`` calls are made before
+        ``compute()`` is called, return an empty tensor.
+
+        Returns:
+            Tuple:
+                - Binned_AUPRC (Tensor): The return value of Binned_AUPRC for each task (num_tasks,).
+                - threshold (Tensor): Tensor of threshold. Its shape is (n_thresholds, ).
+        """
+        return _multiclass_binned_auprc_compute(
+            torch.cat(self.inputs),
+            torch.cat(self.targets),
+            self.num_classes,
+            self.threshold,
+            self.average,
+        )
+
+    @torch.inference_mode()
+    def merge_state(
+        self: TMulticlassBinnedAUPRC, metrics: Iterable[TMulticlassBinnedAUPRC]
+    ) -> TMulticlassBinnedAUPRC:
+        for metric in metrics:
+            if metric.inputs:
+                metric_inputs = torch.cat(metric.inputs).to(self.device)
+                metric_targets = torch.cat(metric.targets).to(self.device)
+                self.inputs.append(metric_inputs)
+                self.targets.append(metric_targets)
+        return self
+
+    @torch.inference_mode()
+    def _prepare_for_merge_state(self: TMulticlassBinnedAUPRC) -> None:
+        if self.inputs and self.targets:
+            self.inputs = [torch.cat(self.inputs)]
+            self.targets = [torch.cat(self.targets)]
