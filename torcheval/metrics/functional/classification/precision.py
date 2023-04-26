Fix error in BinaryPrecision computation (#148)
Summary:
Pull Request resolved: https://github.com/pytorch/torcheval/pull/148

Addressing gh issue https://github.com/pytorch/torcheval/issues/146

Binary Precision doesn't work when target is a float tensor.

Reviewed By: ananthsub

Differential Revision: D45261417

fbshipit-source-id: ab1fce4df33a38d2faab46f22c15b9035eacdade

torcheval/metrics/functional/classification/precision.py
@@ -227,8 +227,8 @@ def _binary_precision_update(
     _binary_precision_update_input_check(input, target)
 
     input = torch.where(input < threshold, 0, 1)
-    num_tp = (input & target).sum()
-    num_fp = (input & (~target)).sum()
+    num_tp = (input * target).sum(dim=-1)
+    num_fp = input.sum(dim=-1) - num_tp
 
     num_label = torch.tensor(0.0)
 