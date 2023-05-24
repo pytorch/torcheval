4ba8d691ef02b53e571159b47961008e95e0c2b1
Gagandeep <gagansingh.iitg@gmail.com>
2023-05-23 21:09:48 -0700

    Added checks for validating distribution

--- torcheval/metrics/functional/statistical/wasserstein.py
+++ torcheval/metrics/functional/statistical/wasserstein.py
@@ -10,7 +10,6 @@ def wasserstein_1d(x: torch.Tensor, y: torch.Tensor,
 
 def _validate_distribution(values: torch.Tensor, weights: torch.Tensor
 ) -> Tuple[torch.Tensor, Optional[torch.Tensor]]:
-    #values = torch.Tensor(values, dtype=torch.float)
 
     if values.nelement() == 0:
         raise ValueError("Distribution cannot be empty.")
@@ -18,8 +17,14 @@ def _validate_distribution(values: torch.Tensor, weights: torch.Tensor
     if weights is None:
         return values, None
     else:
-        #weights = torch.Tensor(weights, dtype=torch.float)
-        pass
+        if weights.nelement() != values.nelement():
+            raise ValueError("Value and weight tensor must be of the same size.")
+        if torch.all(weights < 0):
+            raise ValueError("All weights must be non-negative.")
+        if not ( 0 < torch.sum(weights) < torch.inf ):
+            raise ValueError("Weight tensor sum must be positive-finite.")
+        
+        return values, weights
 
 def _cdf_distribution(p: int, x: torch.Tensor, y: torch.Tensor,
                    x_weights: Optional[torch.Tensor]=None, y_weights: Optional[torch.Tensor]=None
