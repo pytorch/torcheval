a4f167aa2f235dcc0d04a39eeb84df15319f7379
Jason Senthil <jsenthil@meta.com>
2023-06-21 01:49:53 -0700

    remove recipient_rank concept (#159)
    Summary:
    Pull Request resolved: https://github.com/pytorch/torcheval/pull/159
    
    Remove recipient rank as no speedup occurs when syncing to specific rank.
    
    Reviewed By: bobakfb
    
    Differential Revision: D45339353
    
    fbshipit-source-id: ce2bf969ed7173884198f8acaf9b1783b243878e

--- tests/metrics/test_toolkit.py
+++ tests/metrics/test_toolkit.py
@@ -7,7 +7,7 @@
 import os
 import unittest
 import uuid
-from typing import Callable, List, Type, Union
+from typing import Callable, List, Type
 
 import pytest
 
@@ -32,29 +32,16 @@ from torcheval.utils.test_utils.dummy_metric import (
     DummySumMetric,
 )
 from torchtnt.utils import init_from_env
-from typing_extensions import Literal
 
 
 class MetricToolkitTest(unittest.TestCase):
     def test_metric_sync(self) -> None:
         num_processes = torch.cuda.device_count() if torch.cuda.is_available() else 4
         input_tensor = torch.rand(num_processes * 2)
-        # recipient_rank = 0
+
         self._launch_metric_sync_test(num_processes, input_tensor, DummySumMetric)
         self._launch_metric_sync_test(
             num_processes, input_tensor, DummySumListStateMetric
-        )
-        # recipient_rank = 1
-        self._launch_metric_sync_test(num_processes, input_tensor, DummySumMetric, 1)
-        self._launch_metric_sync_test(
-            num_processes, input_tensor, DummySumListStateMetric, 1
-        )
-        # recipient_rank = "all"
-        self._launch_metric_sync_test(
-            num_processes, input_tensor, DummySumMetric, "all"
-        )
-        self._launch_metric_sync_test(
-            num_processes, input_tensor, DummySumListStateMetric, "all"
         )
 
     def test_metric_sync_world_size_1(self) -> None:
@@ -64,23 +51,8 @@ class MetricToolkitTest(unittest.TestCase):
         self.assertDictEqual(metric.state_dict(), synced_metric.state_dict())
 
         self.assertDictEqual(get_synced_state_dict(metric), metric.state_dict())
-        self.assertDictEqual(
-            get_synced_state_dict(metric, recipient_rank="all"), metric.state_dict()
-        )
 
         torch.testing.assert_close(sync_and_compute(metric), metric.compute())
-        torch.testing.assert_close(
-            sync_and_compute(metric, recipient_rank="all"), metric.compute()
-        )
-
-    def test_metric_sync_state_invalid_recipient_rank(self) -> None:
-        metric = DummySumMetric()
-        with self.assertRaisesRegex(
-            ValueError,
-            "``recipient_rank`` should be an integer or 'all', got ALL instead.",
-        ):
-            # pyre-ignore[6]: It's intended to test the behaviour when recipient_rank's value is not correct.
-            get_synced_metric(metric, recipient_rank="ALL")
 
     # pyre-fixmeI[56]: Pyre was not able to infer the type of the decorator `pytest.mark.cpu_and_gpu`.
     @pytest.mark.cpu_and_gpu
@@ -119,7 +91,6 @@ class MetricToolkitTest(unittest.TestCase):
     def _test_per_process_metric_sync(
         input_tensor: torch.Tensor,
         metric_class: Callable[[], Metric],
-        recipient_rank: Union[int, Literal["all"]],
     ) -> None:
         device = init_from_env()
         if device.type == "cuda":
@@ -135,40 +106,34 @@ class MetricToolkitTest(unittest.TestCase):
             metric.update(input_tensor[i])
 
         state_dict_before_sync = metric.state_dict()
-        synced_metric = get_synced_metric(metric, recipient_rank=recipient_rank)
-        compute_result = sync_and_compute(metric, recipient_rank=recipient_rank)
-        synced_state_dict = get_synced_state_dict(metric, recipient_rank=recipient_rank)
+        synced_metric = get_synced_metric(metric)
+        compute_result = sync_and_compute(metric)
+        synced_state_dict = get_synced_state_dict(metric)
 
         # input metric state unchanged
         tc.assertDictEqual(metric.state_dict(), state_dict_before_sync)
 
-        if rank == recipient_rank or recipient_rank == "all":
-            metric_with_all_updates = metric_class().to(device)
-            for i in range(num_total_updates):
-                metric_with_all_updates.update(input_tensor[i])
-            tc.assertIsNotNone(synced_metric)
-            torch.testing.assert_close(
-                synced_metric.compute(),
-                metric_with_all_updates.compute(),
-                check_device=False,
-            )
-            torch.testing.assert_close(
-                compute_result,
-                metric_with_all_updates.compute(),
-                check_device=False,
-            )
-            tc.assertGreater(len(synced_state_dict), 0)
-        else:
-            tc.assertIsNone(synced_metric)
-            tc.assertIsNone(compute_result)
-            tc.assertDictEqual(synced_state_dict, {})
+        metric_with_all_updates = metric_class().to(device)
+        for i in range(num_total_updates):
+            metric_with_all_updates.update(input_tensor[i])
+        tc.assertIsNotNone(synced_metric)
+        torch.testing.assert_close(
+            synced_metric.compute(),
+            metric_with_all_updates.compute(),
+            check_device=False,
+        )
+        torch.testing.assert_close(
+            compute_result,
+            metric_with_all_updates.compute(),
+            check_device=False,
+        )
+        tc.assertGreater(len(synced_state_dict), 0)
 
     def _launch_metric_sync_test(
         self,
         num_processes: int,
         input_tensor: torch.Tensor,
         metric_class: Type[Metric],
-        recipient_rank: Union[int, Literal["all"]] = 0,
     ) -> None:
         lc = pet.LaunchConfig(
             min_nodes=1,
@@ -183,7 +148,6 @@ class MetricToolkitTest(unittest.TestCase):
         pet.elastic_launch(lc, entrypoint=self._test_per_process_metric_sync)(
             input_tensor,
             metric_class,
-            recipient_rank,
         )
 
     def test_reset_metrics(self) -> None:
@@ -238,7 +202,6 @@ class MetricCollectionToolkitTest(unittest.TestCase):
     def _test_per_process_metric_collection_sync(
         input_tensor: torch.Tensor,
         metric_constructors: List[Callable[[], Metric]],
-        recipient_rank: Union[int, Literal["all"]],
     ) -> None:
         device = init_from_env()
         if device.type == "cuda":
@@ -266,15 +229,9 @@ class MetricCollectionToolkitTest(unittest.TestCase):
             metric_names[i]: metrics[metric_names[i]].state_dict()
             for i in range(num_total_metrics)
         }
-        synced_metric_dict = get_synced_metric_collection(
-            metrics, recipient_rank=recipient_rank
-        )
-        compute_result_dict = sync_and_compute_collection(
-            metrics, recipient_rank=recipient_rank
-        )
-        synced_state_dict_dict = get_synced_state_dict_collection(
-            metrics, recipient_rank=recipient_rank
-        )
+        synced_metric_dict = get_synced_metric_collection(metrics)
+        compute_result_dict = sync_and_compute_collection(metrics)
+        synced_state_dict_dict = get_synced_state_dict_collection(metrics)
 
         # input metric state unchanged
         tc.assertDictEqual(
@@ -285,53 +242,46 @@ class MetricCollectionToolkitTest(unittest.TestCase):
             state_dicts_before_sync,
         )
 
-        if rank == recipient_rank or recipient_rank == "all":
+        # make sure we get real dicts on all ranks
+        tc.assertIsNotNone(synced_metric_dict)
+        tc.assertIsNotNone(compute_result_dict)
+        tc.assertIsNotNone(synced_state_dict_dict)
 
-            # make sure we get real dicts on recipient ranks
-            tc.assertIsNotNone(synced_metric_dict)
-            tc.assertIsNotNone(compute_result_dict)
-            tc.assertIsNotNone(synced_state_dict_dict)
+        # construct new empty metrics
+        metrics_with_all_updates = {
+            metric_names[i]: metric_constructors[i]().to(device)
+            for i in range(num_total_metrics)
+        }
 
-            # construct new empty metrics
-            metrics_with_all_updates = {
-                metric_names[i]: metric_constructors[i]().to(device)
-                for i in range(num_total_metrics)
-            }
+        # update all the metrics in the collection so that we have a copy of the
+        # metrics with all the updates applied on one rank, bypassing syncing logic.
+        for i in range(num_total_updates):
+            for j in range(num_total_metrics):
+                metrics_with_all_updates[metric_names[j]].update(input_tensor[i, j])
 
-            # update all the metrics in the collection so that we have a copy of the
-            # metrics with all the updates applied on one rank, bypassing syncing logic.
-            for i in range(num_total_updates):
-                for j in range(num_total_metrics):
-                    metrics_with_all_updates[metric_names[j]].update(input_tensor[i, j])
-
-            # test that each of the metrics constructed on one rank without syncing have
-            # the same compute() value as the metrics updated on different ranks and synced
-            # on this rank.
-            for metric_id in metric_names:
-                # test through the get_synced_metric_collection route
-                torch.testing.assert_close(
-                    synced_metric_dict[metric_id].compute(),
-                    metrics_with_all_updates[metric_id].compute(),
-                    check_device=False,
-                )
-                # test through the synced_and_compute_collection route
-                torch.testing.assert_close(
-                    compute_result_dict[metric_id],
-                    metrics_with_all_updates[metric_id].compute(),
-                    check_device=False,
-                )
-                tc.assertGreater(len(synced_state_dict_dict[metric_id]), 0)
-        else:
-            tc.assertIsNone(synced_metric_dict)
-            tc.assertIsNone(compute_result_dict)
-            tc.assertIsNone(synced_state_dict_dict)
+        # test that each of the metrics constructed on one rank without syncing have
+        # the same compute() value as the metrics updated on different ranks and synced
+        # on this rank.
+        for metric_id in metric_names:
+            # test through the get_synced_metric_collection route
+            torch.testing.assert_close(
+                synced_metric_dict[metric_id].compute(),
+                metrics_with_all_updates[metric_id].compute(),
+                check_device=False,
+            )
+            # test through the synced_and_compute_collection route
+            torch.testing.assert_close(
+                compute_result_dict[metric_id],
+                metrics_with_all_updates[metric_id].compute(),
+                check_device=False,
+            )
+            tc.assertGreater(len(synced_state_dict_dict[metric_id]), 0)
 
     def _launch_collection_sync_test(
         self,
         num_processes: int,
         input_tensor: torch.Tensor,
         metric_classes: List[Type[Metric]],
-        recipient_rank: Union[int, str] = 0,
     ) -> None:
         lc = pet.LaunchConfig(
             min_nodes=1,
@@ -348,7 +298,6 @@ class MetricCollectionToolkitTest(unittest.TestCase):
         )(
             input_tensor,
             metric_classes,
-            recipient_rank,
         )
 
     def test_metric_collection_sync(self) -> None:
@@ -357,39 +306,16 @@ class MetricCollectionToolkitTest(unittest.TestCase):
         num_updates = 3 * num_processes
 
         input_tensor = torch.rand(size=(num_metrics, num_updates))
-        # recipient_rank = 0
-        self._launch_collection_sync_test(
-            num_processes, input_tensor, [DummySumMetric for i in range(num_metrics)]
-        )
-        self._launch_collection_sync_test(
-            num_processes,
-            input_tensor,
-            [DummySumListStateMetric for i in range(num_metrics)],
-        )
 
-        # recipient_rank = 1
-        self._launch_collection_sync_test(
-            num_processes, input_tensor, [DummySumMetric for i in range(num_metrics)], 1
-        )
-        self._launch_collection_sync_test(
-            num_processes,
-            input_tensor,
-            [DummySumListStateMetric for i in range(num_metrics)],
-            1,
-        )
-
-        # recipient_rank = "all"
         self._launch_collection_sync_test(
             num_processes,
             input_tensor,
             [DummySumMetric for i in range(num_metrics)],
-            "all",
         )
         self._launch_collection_sync_test(
             num_processes,
             input_tensor,
             [DummySumListStateMetric for i in range(num_metrics)],
-            "all",
         )
 
     def test_metric_collection_sync_world_size_1(self) -> None:
@@ -413,6 +339,6 @@ class MetricCollectionToolkitTest(unittest.TestCase):
         )
         self.assertDictEqual(
             # pyre-ignore: Incompatible parameter type [6]: In call `unittest.case.TestCase.assertDictEqual`, for 1st positional only parameter expected `Mapping[typing.Any, object]` but got `Optional[Dict[str, Dict[str, typing.Any]]]`.
-            get_synced_state_dict_collection(metric_collection, recipient_rank="all"),
+            get_synced_state_dict_collection(metric_collection),
             state_dict_collection,
         )
