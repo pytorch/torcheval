Fix integer overflow in Covariance
Summary:
Previously, we did:

```
r0 = outer * (n * self.n)
r1 = r0 / (self.n + n)
```

This causes an integer overflow for large `self.n` because when PyTorch needs to cast forom a Python `int` (which is variably size) to a `torch.int64` (which has finite range).

This fixes the problem by reversing the order, to do:
```
r0 = (n * self.n) / (self.n + n)
r1 = outer * r0
```

This should also be marginally faster, since we replace 1 tensor operation with a scalar operation.

Reviewed By: zaccharieramzi, tanviranjanmeta

Differential Revision: D80752331

fbshipit-source-id: c1c84b79225d565115e14b8323477a11cccd32ff

torcheval/metrics/aggregation/cov.py
@@ -47,7 +47,9 @@ class Covariance(Metric[_Output]):
             # Welford's algorithm for numerical stability
             delta = (self.sum / self.n) - (sum / n)
             outer = torch.outer(delta, delta)
-            self.ss_sum += ss_sum + outer * (n * self.n) / (self.n + n)
+
+            scale = n * self.n / (self.n + n)
+            self.ss_sum += ss_sum + outer * scale
             self.sum += sum
             self.n += n
 